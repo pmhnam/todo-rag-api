f844a1724559e12be74ec999374f9becd97ee5ae
otis <otis@vinova.com.sg>
2026-05-16 14:54:44 +0700

    feat(tests): enhance Jest configuration and update e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,28 @@
 import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { HomeModule } from './../src/api/home/home.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [HomeModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
+  afterAll(async () => {
+    await app.close();
+  });
+
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome to the API');
   });
 });
